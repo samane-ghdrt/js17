f134a4f300e8bec6d34ece8e2a22902c969c6bf7
samane ghodrati <samane.ghodrati@gmail.com>
2025-01-16 00:04:10 -0900

    add

--- 1-todo-list/script.js
+++ 1-todo-list/script.js
@@ -1,5 +1,21 @@
+const divEl =document.querySelector('#content');
 function todoList(todos) {
   // Write your code here...
+  const ulEl=document.createElement('ul');
+  divEl.append(ulEl);
+  
+  
+  for (const element of todos) {
+    const liEl=document.createElement('li');
+    ulEl.append(liEl);
+    
+    for (const [key, value] of Object.entries(element)) {
+      liEl.textContent=key +':' +value ;
+    }
+  }
+divEl.addEventListener('click',(e)=>{
+e.target.style
+})
 }
 
 const todos = [
@@ -8,5 +24,6 @@ const todos = [
   {todo: "learn javascript"}, 
   {todo: "go shopping"}
 ];
+ 
 
 todoList(todos);
